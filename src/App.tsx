08094e928cff8f94fa4accb5501591214e4c5d11
vimcs <vidalmarcos11@gmail.com>
2025-07-30 10:45:15 -0300

    Protegendo os atributos e testando a proteção

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { Usuario } from './models/Usuario'
 import './App.css'
 
 function App() {
-  
+ 
+const u2 = new Usuario('Flavin do Pnel' , 54);
 const ul = new Usuario('João da silva', 35);
+
+ul.nome = 'Marcos';
 
   return (
     <>
       <p>{ul.apresentar()}</p>
+      <p>{u2.apresentar()}</p>
     </>
   )
 }
--- src/models/Usuario.ts
+++ src/models/Usuario.ts
@@ -1,13 +1,15 @@
 export class Usuario {
-  nome: string;
-  idade: number;
+  #nome: string;
+  #idade: number;
 
   constructor(nome: string, idade: number) {
-    this.nome = nome;
-    this.idade = idade;
+    this.#nome = nome;
+    this.#idade = idade;
   }
 
   apresentar(): string {
-    return `Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`;
+    return `Olá, meu nome é ${this.#nome} e tenho ${this.#idade} anos.`;
   }
+
+
 }
