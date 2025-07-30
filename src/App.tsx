3d53bc86627c25d420aad35ebc877704df4b70a8
vimcs <vidalmarcos11@gmail.com>
2025-07-30 10:19:24 -0300

    Criada a Classe Usuario

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-
+import { Usuario } from './models/Usuario'
 import './App.css'
 
 function App() {
   
+const ul = new Usuario('João da silva', 35);
 
   return (
     <>
-      
+      <p>{ul.apresentar()}</p>
     </>
   )
 }
---
+++ src/models/Usuario.ts
@@ -0,0 +1,13 @@
+export class Usuario {
+  nome: string;
+  idade: number;
+
+  constructor(nome: string, idade: number) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+
+  apresentar(): string {
+    return `Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`;
+  }
+}
